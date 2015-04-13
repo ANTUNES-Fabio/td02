2148da023d5b2372c32ead6e2cda1147e7cd2f24
Fabio Antunes <fabio.antunes@depinfonancy.net>
2015-04-13 19:20:26 +0200

    Interface "Complete".

--- td02/src/org/depinfonancy/td02/Td02UI.java
+++ td02/src/org/depinfonancy/td02/Td02UI.java
@@ -7,6 +7,8 @@ import javax.naming.Context;
 import javax.servlet.annotation.WebServlet;
 import javax.ejb.EJB;
 
+import java.util.List;
+
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinRequest;
@@ -15,6 +17,7 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextArea;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
@@ -58,32 +61,126 @@ public class Td02UI extends UI {
 		
 		//Sets up the bottom part of the page with two lines of buttons and one for the return message.
 		final VerticalLayout bottom = new VerticalLayout();
-		layout.addComponent(bottom);
-		
-		//First Line.
+		layout.addComponent(bottom);	
+
 		final HorizontalLayout bottom1 = new HorizontalLayout();
 		bottom.addComponent(bottom1);
+		final HorizontalLayout bottom2 = new HorizontalLayout();
+		bottom.addComponent(bottom2);
+		final VerticalLayout bottom3 = new VerticalLayout();
+		bottom.addComponent(bottom3);
 		
+		//First Line.
 		Button createStudent = new Button("Create Student");
 		createStudent.addClickListener(new Button.ClickListener() {
 			
 			@Override
 			public void buttonClick(ClickEvent event) {
-				String returnMessage;
+				String returnMessage = null;
 				try{
 					int age = Integer.parseInt(studentAge.getValue());
 					students.createStudent(new Student(studentName.getValue(), studentAddress.getValue(), age));
-					returnMessage = "Student added successfully";
+					returnMessage = "Student successfully added.";
 				}catch(Exception e){
 					System.out.println(e);
-					returnMessage = "An error ocurred, check if you inserted a valid age value on the Student Age box";
+					returnMessage = "An error ocurred, check if you inserted a valid age value on the Student Age box.";
 				}
-				System.out.println(returnMessage);
-				
-				
+				bottom3.removeAllComponents();
+				bottom3.addComponent(new TextArea(returnMessage));
 			}
 		});
 		bottom1.addComponent(createStudent);
+		
+		Button updateStudent = new Button("Update Student");
+		updateStudent.addClickListener(new Button.ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				String returnMessage;
+				try{
+					int id = Integer.parseInt(studentID.getValue());
+					int age = Integer.parseInt(studentAge.getValue());
+					Student TempStudent = new Student(studentName.getValue(), studentAddress.getValue(), age);
+					TempStudent.setStudentID(id);
+					students.updateStudent(TempStudent);
+					returnMessage = "Student successfully updated.";
+				}catch(Exception e){
+					System.out.println(e);
+					returnMessage = "An error ocurred, check if you inserted valid age and id values.";
+				}
+				bottom3.removeAllComponents();
+				bottom3.addComponent(new TextArea(returnMessage));
+			}
+		});
+		bottom1.addComponent(updateStudent);
+		
+		Button deleteStudent = new Button("Delete Student");
+		updateStudent.addClickListener(new Button.ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				String returnMessage;
+				try{
+					int id = Integer.parseInt(studentID.getValue());
+					Student TempStudent = new Student("", "", 0);
+					TempStudent.setStudentID(id);
+					students.deleteStudent(students.findStudentById(TempStudent));
+					returnMessage = "Student successfully deleted.";
+				}catch(Exception e){
+					System.out.println(e);
+					returnMessage = "An error ocurred, check if you inserted a valid id value.";
+				}
+				bottom3.removeAllComponents();
+				bottom3.addComponent(new TextArea(returnMessage));
+			}
+		});
+		bottom1.addComponent(deleteStudent);
+		
+		Button findStudentByID = new Button("Find Student By ID");
+		findStudentByID.addClickListener(new Button.ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				String returnMessage;
+				try{
+					int id = Integer.parseInt(studentID.getValue());
+					Student TempStudent = new Student("", "", 0);
+					TempStudent.setStudentID(id);
+					returnMessage = studentToString(students.findStudentById(TempStudent));
+					
+				}catch(Exception e){
+					System.out.println(e);
+					returnMessage = "An error ocurred, check if you inserted a valid id value.";
+				}
+				bottom3.removeAllComponents();
+				bottom3.addComponent(new TextArea(returnMessage));
+			}
+		});
+		bottom2.addComponent(findStudentByID);
+		
+		Button findStudents = new Button("Find All Students");
+		findStudents.addClickListener(new Button.ClickListener() {
+			
+			@Override
+			public void buttonClick(ClickEvent event) {
+				List<Student> allStudents = null;
+				try {
+					allStudents = students.findStudents();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				bottom3.removeAllComponents();
+				if(allStudents.size() == 0){
+					bottom3.addComponent(new TextArea("There aren't students registered."));
+				}else{
+					for(Student s : allStudents){
+						bottom3.addComponent(new TextArea(studentToString(s)));
+					}
+				}	
+			}
+		});
+		bottom2.addComponent(findStudents);
+		
 		
 		//
 //		Button button = new Button("Click Me");
@@ -94,7 +191,21 @@ public class Td02UI extends UI {
 //		});
 //		layout.addComponent(button);
 	}
-
+	
+	public String studentToString(Student student){
+		String returnMessage;
+		if(student == null){
+			returnMessage = "No student found.";
+		}else{
+			returnMessage = student.getName() + "\n"
+					+ "ID: " + student.getStudentID() + "\n"
+					+ "Address:" + student.getAddress() + "\n"
+							+ "Age: " + student.getAge();
+		}
+		
+		return returnMessage;
+	}
+	
 	public StudentEJBRemote setup() throws Exception{
 		
 		final Properties p = new Properties();
